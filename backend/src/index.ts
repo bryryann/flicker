21424aa236701771a59e60e87a7da809a1d34b00
hazmatx <186111776+hazmatx@users.noreply.github.com>
2024-12-09 15:49:18 -0300

    Configure local HTTPS protocol on Express server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,17 @@
+import https from "https";
+import fs from "fs";
+import path from "path";
 import express, { Request, Response } from "express";
 import cors from "cors";
 import config from "./utils/config";
 import usersRouter from "./routers/users-router";
 
+const options = {
+    cert: fs.readFileSync(path.dirname(__dirname) + "/src/cert/certificate.pem"),
+    key: fs.readFileSync(path.dirname(__dirname) + "/src/cert/private-key.pem"),
+};
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
@@ -11,4 +19,6 @@ app.get("/", (_req: Request, res: Response) => { res.json({ version: '0.0.1' }) 
 
 app.use("/users", usersRouter);
 
-app.listen(config.PORT, () => console.log(`server listening on port ${config.PORT}`));
+https.createServer(options, app).listen(config.PORT, () => {
+    console.log(`server listening on https://localhost:${config.PORT}`);
+});
