8a6de4b5cd389d8a9281ec468574ad83efa10366
hazmatx <186111776+hazmatx@users.noreply.github.com>
2024-12-21 09:45:08 -0300

    build(client): set up Vite proxy for third-party api calls

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,14 @@ import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://api.themoviedb.org/3",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
   plugins: [mkcert(), react()],
 });
