3897c5d3ba6d9412396bc880abc7af5ac5b039a9
hazmatx <186111776+hazmatx@users.noreply.github.com>
2024-12-20 21:17:43 -0300

    feat(ui): styled sidebar
    - Changed color palette(again...)

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -5,12 +5,13 @@ const Sidebar: React.FC = () => {
         <div className="main-sidebar">
             <ul className="sidebar-anchors">
                 <a href="#">Favorites (W.I.P)</a>
-                <hr />
+                <br />
                 <a href="#">Watchlist (W.I.P)</a>
-                <hr />
-                <a href="#">Profile (W.I.P)</a>
+                <br />
             </ul>
-
+            <div className="sidebar-profile">
+                <a href="#">My Profile (W.I.P)</a>
+            </div>
         </div>
     )
 }
